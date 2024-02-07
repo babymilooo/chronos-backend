94d645d0f88797d7beffa212ffbb4fa2bec6347e
babymilooo <Kyrylo.Yehorov@cit.khpi.edu.ua>
2024-02-07 21:57:27 +0200

    Feature: controllers, models, router

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
+const router = require('./router/index')
+
 const PORT = process.env.PORT || 5000;
 
 const app = express();
@@ -15,6 +17,7 @@ app.use(cors({
 
 app.use(cookieParser());
 app.use(express.json());
+app.use('/api', router);
 
 const start = async () => {
     try {
