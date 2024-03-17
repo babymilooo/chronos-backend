20b866a86cce129539f36366294ddfe513dafe03
babymilooo <Kyrylo.Yehorov@cit.khpi.edu.ua>
2024-03-17 15:11:52 +0200

    Fix: add email in user dto

--- dtos/user-dto.js
+++ dtos/user-dto.js
@@ -3,10 +3,12 @@ module.exports = class UserDto {
     bio;
     username;
     image;
+    email;
     constructor(model) {
         this.id = model._id;
         this.bio = model.bio;
         this.username = model.username;
         this.image = model.image;
+        this.email = model.email;
     }
 }
